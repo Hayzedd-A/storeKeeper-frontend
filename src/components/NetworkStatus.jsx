a559c581227f0bf86f20787925157b97e04fcc22
Azeez ADEBAYO <adebayoazeez37@yahoo.com>
2024-09-19 18:06:02 +0100

    added server condition info as a marque to the network alert

--- src/components/NetworkStatus.jsx
+++ src/components/NetworkStatus.jsx
@@ -1,19 +1,42 @@
-import { Alert } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Alert, Space, Spin } from "antd";
 import React from "react";
-
+import Marquee from "react-fast-marquee";
 function NetworkStatus({ showActionButton, alertMessage, alertType }) {
+  console.log(alertType);
+  const actionButton = () => {
+    if (alertType === "info") {
+      console.log("if is running");
+      return <Spin indicator={<LoadingOutlined spin />} size="small" />;
+    } else if (alertType === "success") return false;
+    else {
+      console.log("else is running");
+      return (
+        <a type="primary" onClick={() => window.location.reload()}>
+          Reload
+        </a>
+      );
+    }
+  };
   return (
-    <div className="header">
+    <div id="network-alert" className="header">
       <Alert
         message={alertMessage}
         type={alertType}
         // closable
         action={
-          showActionButton && (
-            <a type="primary" onClick={() => window.location.reload()}>
-              Reload
-            </a>
-          )
+          <span
+            style={{
+              display: "flex",
+            }}
+          >
+            <Marquee gradient={false} pauseOnHover>
+              " ------------ It could take a while to get data from the server.
+              The backend server is hosted on a free server which shuts down
+              after some while of inactivity. Please take some patient"
+            </Marquee>
+            {actionButton()}
+          </span>
         }
       />
     </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,20 @@
 import { AutoComplete } from "antd";
 import { Input } from "antd";
-import React, { useState } from "react";
+import React, { createRef, useEffect, useState } from "react";
 const { Search } = Input;
 
 function SearchBar({ setSearchKeyword }) {
   const [searchValue, setSearchValue] = useState("");
+  const inputRef = createRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   return (
     <div className="search">
       <AutoComplete autoComplete="true" allowClear={true}>
         <Search
+          ref={inputRef}
           placeholder="Search available products"
           onChange={e => {
             setSearchKeyword(e.target.value);
